18a13746259efc8712c3299925683785ce8352df
Heema Rajkarnikar <heemar@hotmail.com>
2018-05-27 06:24:02 -0400

    googlemap pod and initial map code

---
+++ Event Map/Controller/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Event Map
+//
+//  Created by bjoshi on 5/26/18.
+//  Copyright © 2018 hrajkarnikar. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '9.0'
+
+target 'Event Map' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Event Map
+pod 'GoogleMaps'
+
+end
